3e41a9436049954d2d4f07a05987b5c7dc378acb
Albeiro Cuadrado M <machado.albeiro@gmail.com>
2022-11-27 21:15:33 -0500

    init main

---
+++ android/app/src/main/kotlin/com/example/dermosolution_usability_experiment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dermosolution_usability_experiment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:body_part_selector/body_part_selector.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Usability experiment - DermoSolution',
+      home: MyHomePage(title: "Body part selector"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BodyParts _bodyParts = const BodyParts();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: BodyPartSelectorTurnable(
+          bodyParts: _bodyParts,
+          onSelectionUpdated: (p) => setState(() => _bodyParts = p),
+          labelData: const RotationStageLabelData(
+            front: 'FRENTE',
+            left: 'IZQ',
+            right: 'DER',
+            back: 'ATRAS',
+          ),
+        ),
+      ),
+    );
+  }
+}
